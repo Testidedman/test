f8bf60274dd600cb4fda5040ad3eec0bd7bcc2f0
TagidickTagidick <49733518+TagidickTagidick@users.noreply.github.com>
2025-03-05 18:45:33 +0300

    fix bugs

--- lib/services/remote_config_service/firebase_remote_config_service.dart
+++ lib/services/remote_config_service/firebase_remote_config_service.dart
@@ -1,26 +1,30 @@
 import 'dart:convert';
-
 import 'package:firebase_remote_config/firebase_remote_config.dart';
-import 'package:package_info_plus/package_info_plus.dart';
 import 'package:test_app/models/version_model.dart';
+import 'package:test_app/services/loading_service.dart';
 import 'package:test_app/services/remote_config_service/remote_config_service.dart';
 
 class FirebaseRemoteConfigService implements RemoteConfigService {
   @override
   Future<void> init() async {
-    final remoteConfig = FirebaseRemoteConfig.instance;
-    await remoteConfig.setConfigSettings(RemoteConfigSettings(
-      fetchTimeout: const Duration(minutes: 1),
-      minimumFetchInterval: const Duration(hours: 1),
-    ));
-    await remoteConfig.fetchAndActivate();
+    try {
+      final remoteConfig = FirebaseRemoteConfig.instance;
+      await remoteConfig.setConfigSettings(RemoteConfigSettings(
+        fetchTimeout: const Duration(minutes: 1),
+        minimumFetchInterval: const Duration(hours: 1),
+      ));
+      await remoteConfig.fetchAndActivate();
+    }
+    catch (_) {
+      rethrow;
+    }
   }
 
   @override
   Future<AppStatus> getAppStatus() async {
     try {
       final List<VersionModel> versionModels = _getVersions();
-      final String currentVersion = await _getCurrentVersion();
+      final String currentVersion = await LoadingService.getCurrentVersion();
       final VersionModel currentVersionModel = versionModels.firstWhere(
               (versionModel) => currentVersion == versionModel.version
       );
@@ -36,16 +40,6 @@ class FirebaseRemoteConfigService implements RemoteConfigService {
       final remoteConfig = FirebaseRemoteConfig.instance;
       final response = remoteConfig.getString('GooglePlay');
       return VersionModel.listFromJson(jsonDecode(response));
-    }
-    catch (_) {
-      rethrow;
-    }
-  }
-
-  static Future<String> _getCurrentVersion() async {
-    try {
-      final PackageInfo packageInfo = await PackageInfo.fromPlatform();
-      return packageInfo.version;
     }
     catch (_) {
       rethrow;
